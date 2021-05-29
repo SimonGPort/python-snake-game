469f3d44d6a0b2a5e66c69c9b8386fe2a6191cc7
SimonGPort <simon.giroux.portelance@gmail.com>
2021-05-29 17:34:35 -0400

    fist collision game_over

--- snake.py
+++ snake.py
@@ -21,6 +21,7 @@ BLACK=(0,0,0)
 
 class SnakeGame:
     def __init__(self,w=640,h=480):
+        self.game_over=False
         self.w=w
         self.h=h
         self.display=pygame.display.set_mode((self.w,self.h))
@@ -45,7 +46,7 @@ class SnakeGame:
             self.food=foodPosition
 
     def play_step(self):
-#1 collect user input
+        #1 collect user input
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
@@ -60,13 +61,12 @@ class SnakeGame:
                 if event.key==pygame.K_DOWN:
                     self.direction='DOWN'
 
-#2 move
+        #2 move
         self.move()
-#3 check if game over
-        game_over=False
+        #3 check if game over
         if self.collision():
-            game_over=True
-#4 place new food or just move
+            self.game_over=True
+        #4 place new food or just move
         # self.placeFood()
         if self.body[0]==self.food:
             DirectionTailValue=self.body[-1]-self.body[-2]
@@ -98,66 +98,42 @@ class SnakeGame:
 
         
 
-#5 update ui and clock
+        #5 update ui and clock
         self.updateUi()
         self.clock.tick(SPEED)
 
-#6 return game over and score
+        #6 return game over and score
         
-        return game_over,self.score
+        return self.game_over,self.score
 
     def collision(self):
-        #hit the boundary
-            #border du haut
-        if self.body[0]<1:
-            return True
-            #border du bas
-        if self.body[0]>768:
-            return True
-            #border du droite et gauche
-        # rowFirstBodyPart=math.floor(self.body[0]/32)
-        # if self.body[0]%32==0:
-        #     rowFirstBodyPart-=1
-
-        firstCol=[1,33,65,97,129,161,193,225,257,289,321,353,385,417,449,481,513,545,577,609,641,673,705,737]
-        lastCol=[32,64,96,128,160,192,224,256,288,320,352,384,416,448,480,512,544,576,608,640,672,704,736,768]
-        isFirstBodyPartFirstCol=False
-        isFirstBodyPartLastCol=False
-        if self.body[0] in firstCol:
-            isFirstBodyPartFirstCol=True
-        if self.body[0] in lastCol:
-            isFirstBodyPartLastCol=True
-
-        # rowSecondBodyPart=math.floor(self.body[1]/32)
-        # if self.body[1]%32==0:
-        #     rowSecondBodyPart-=1
-
-        isSecondBodyPartFirstCol=False
-        isSecondBodyPartLastCol=False
-        if self.body[1] in firstCol:
-            isSecondBodyPartFirstCol=True
-        if self.body[1] in lastCol:
-            isSecondBodyPartLastCol=True
-
-        # notTheSameRow= rowFirstBodyPart != rowSecondBodyPart
-        notTheSameCol=False
-        if isSecondBodyPartLastCol==True and isSecondBodyPartLastCol==True:
-            notTheSameCol=True
-
-        if isFirstBodyPartFirstCol==True and isFirstBodyPartLastCol==True:
-            notTheSameCol=True
-
-        if notTheSameCol:
-            return True
-
         #hit itself
-        if self.body[0] in self.body[1:]:
-            return True
-        return False
+        return self.body[0] in self.body[1:]
 
     def move(self):
         direction=self.direction
         body=self.body.copy()
+
+        #verification si le mouvement est a l'exterieur
+        firstCol=[1,33,65,97,129,161,193,225,257,289,321,353,385,417,449,481,513,545,577,609,641,673,705,737]
+        lastCol=[32,64,96,128,160,192,224,256,288,320,352,384,416,448,480,512,544,576,608,640,672,704,736,768]
+        topCol=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+        downCol=[737,738,739,740,741,742,743,744,745,746,745,746,747,748,749,750,751,752,753,754,755,756,757,758,759,760,761,762,763,764,765,766,767,768]
+
+        if direction=='RIGHT' and self.body[0] in lastCol:
+            self.game_over=True
+            return
+        if direction=='LEFT' and self.body[0] in firstCol:
+            self.game_over=True
+            return
+        if direction=='UP' and self.body[0] in topCol:
+            self.game_over=True
+            return
+        if direction=='DOWN' and self.body[0] in downCol:
+            self.game_over=True
+            return
+        print("pass",body[0])
+
         if direction=='RIGHT':
             body[0]=body[0]+1
         if direction=='LEFT':
